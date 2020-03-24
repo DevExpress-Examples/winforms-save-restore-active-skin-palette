e17276207ed5e87da1cae9f20920bf31c1d1c05b
Pabloid11 <pavel.korovkin@gmail.com>
2020-03-24 14:07:09 +0300

    Updated example code

--- CS/DXApplication1/Form1.cs
+++ CS/DXApplication1/Form1.cs
@@ -15,67 +15,46 @@ namespace DXApplication1
 {
     public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm
     {
-        public Form1()
-        {
+        public Form1() {
             InitializeComponent();
             this.FormClosing += Form1_FormClosing;
         }
 
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
             SavePalette();
         }
 
-        private void SetSkin(string skinName)
-        {
-            UserLookAndFeel.Default.SetSkinStyle(skinName);
-        }
-
-        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
-        {
+        private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
             ShowSwatchPicker(this);
         }
 
-        private void ShowSwatchPicker(Form owner)
-        {
-            using (var dialog = new DevExpress.Customization.SvgSkinPaletteSelector(owner))
-            {
+        private void ShowSwatchPicker(Form owner) {
+            using(var dialog = new DevExpress.Customization.SvgSkinPaletteSelector(owner)) {
                 dialog.ShowDialog();
                 SavePalette();
             }
         }
 
-        private void SavePalette()
-        {
-            Settings.Default["SkinName"] = UserLookAndFeel.Default.SkinName;
-            if (UserLookAndFeel.Default.SkinName == "The Bezier")
-            {
-                Settings.Default["Palette"] = UserLookAndFeel.Default.ActiveSvgPaletteName;
-            }
-            Settings.Default.Save();
+        private void SavePalette() {
+            var settings = Properties.Settings.Default;
+            settings.SkinName = UserLookAndFeel.Default.SkinName;
+            settings.Palette = UserLookAndFeel.Default.ActiveSvgPaletteName;
+            settings.Save();
         }
 
-        protected override void OnShown(EventArgs e)
-        {
+        protected override void OnShown(EventArgs e) {
             base.OnShown(e);
             RestorePalette();
         }
-        
-        private void RestorePalette()
-        {
-            if (Settings.Default["SkinName"] != null && Settings.Default["SkinName"].ToString() != string.Empty)
-            {
-                if (Settings.Default["SkinName"].ToString() == "The Bezier" && Settings.Default["Palette"] != null && Settings.Default["Palette"].ToString() != string.Empty)
-                {
-                    UserLookAndFeel.Default.SetSkinStyle(Settings.Default["SkinName"].ToString(), Settings.Default["Palette"].ToString());
+
+        private void RestorePalette() {
+            var settings = Properties.Settings.Default;
+            if(!String.IsNullOrEmpty(settings.SkinName)) {
+                if(!String.IsNullOrEmpty(settings.SkinName)) {
+                    UserLookAndFeel.Default.SetSkinStyle(settings.SkinName, settings.Palette);
                 }
                 else
-                    SetSkin(Settings.Default["SkinName"].ToString());
-
-            }
-            else if (Settings.Default["Palette"] != null && Settings.Default["Palette"].ToString() != string.Empty)
-            {
-                UserLookAndFeel.Default.SetSkinStyle(SkinStyle.Bezier, Settings.Default["Palette"].ToString());
+                    UserLookAndFeel.Default.SetSkinStyle(settings.SkinName);
             }
         }
     }
