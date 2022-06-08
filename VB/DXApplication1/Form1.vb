d999c3e316599f4730f1b07dcd4bb5dda8a011d7
DevExpressExampleBot <fakeEmail>
2022-06-08 15:43:21 +0400

    Source auto update [skip ci]

--- VB/DXApplication1/Form1.vb
+++ VB/DXApplication1/Form1.vb
@@ -1,62 +1,56 @@
-﻿Imports DevExpress.LookAndFeel
-Imports DevExpress.Skins
-Imports DevExpress.Utils.Svg
+Imports DevExpress.LookAndFeel
+Imports DXApplication1.Properties
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Linq
-Imports System.Text
 Imports System.Windows.Forms
 
 Namespace DXApplication1
-	Partial Public Class Form1
-		Inherits DevExpress.XtraBars.Ribbon.RibbonForm
 
-		Public Sub New()
-			InitializeComponent()
-			AddHandler Me.FormClosing, AddressOf Form1_FormClosing
-		End Sub
+    Public Partial Class Form1
+        Inherits DevExpress.XtraBars.Ribbon.RibbonForm
 
-		Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs)
-			SavePalette()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            AddHandler FormClosing, AddressOf Form1_FormClosing
+        End Sub
 
-		Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles barButtonItem1.ItemClick
-			ShowSwatchPicker(Me)
-		End Sub
+        Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs)
+            SavePalette()
+        End Sub
 
-		Private Sub ShowSwatchPicker(ByVal owner As Form)
-			Using dialog = New DevExpress.Customization.SvgSkinPaletteSelector(owner)
-				dialog.ShowDialog()
-				SavePalette()
-			End Using
-		End Sub
+        Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs)
+            ShowSwatchPicker(Me)
+        End Sub
 
-		Private Sub SavePalette()
-'INSTANT VB NOTE: The variable settings was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim settings_Conflict = My.Settings.Default
-			settings_Conflict.SkinName = UserLookAndFeel.Default.SkinName
-			settings_Conflict.Palette = UserLookAndFeel.Default.ActiveSvgPaletteName
-			settings_Conflict.Save()
-		End Sub
+        Private Sub ShowSwatchPicker(ByVal owner As Form)
+            Using dialog = New DevExpress.Customization.SvgSkinPaletteSelector(owner)
+                dialog.ShowDialog()
+                SavePalette()
+            End Using
+        End Sub
 
-		Protected Overrides Sub OnShown(ByVal e As EventArgs)
-			MyBase.OnShown(e)
-			RestorePalette()
-		End Sub
+        Private Sub SavePalette()
+            Dim settings = Properties.Settings.Default
+            settings.SkinName = UserLookAndFeel.Default.SkinName
+            settings.Palette = UserLookAndFeel.Default.ActiveSvgPaletteName
+            settings.Save()
+        End Sub
 
-		Private Sub RestorePalette()
-'INSTANT VB NOTE: The variable settings was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
-			Dim settings_Conflict = My.Settings.Default
-			If Not String.IsNullOrEmpty(settings_Conflict.SkinName) Then
-				If Not String.IsNullOrEmpty(settings_Conflict.SkinName) Then
-					UserLookAndFeel.Default.SetSkinStyle(settings_Conflict.SkinName, settings_Conflict.Palette)
-				Else
-					UserLookAndFeel.Default.SetSkinStyle(settings_Conflict.SkinName)
-				End If
-			End If
-		End Sub
-	End Class
+        Protected Overrides Sub OnShown(ByVal e As EventArgs)
+            MyBase.OnShown(e)
+            RestorePalette()
+        End Sub
+
+        Private Sub RestorePalette()
+            Dim settings = Properties.Settings.Default
+            If Not String.IsNullOrEmpty(settings.SkinName) Then
+                If Not String.IsNullOrEmpty(settings.SkinName) Then
+                    UserLookAndFeel.Default.SetSkinStyle(settings.SkinName, settings.Palette)
+                Else
+                    UserLookAndFeel.Default.SetSkinStyle(settings.SkinName)
+                End If
+            End If
+        End Sub
+    End Class
 End Namespace
--- VB/DXApplication1/Program.vb
+++ VB/DXApplication1/Program.vb
@@ -1,23 +1,22 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.UserSkins
 Imports DevExpress.Skins
 
 Namespace DXApplication1
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
 
-			BonusSkins.Register()
-			SkinManager.EnableFormSkins()
-			Application.Run(New Form1())
-		End Sub
-	End Module
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            Call BonusSkins.Register()
+            Call SkinManager.EnableFormSkins()
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
